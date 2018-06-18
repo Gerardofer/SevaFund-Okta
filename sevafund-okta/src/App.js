945ae453edb2d4b367c12e96f3c8aa385cecad84
Gerardo Fernandez <gerardojose13@gmail.com>
2018-06-18 21:43:15 +0000

    basic setup for okta auth

--- sevafund-okta/src/App.js
+++ sevafund-okta/src/App.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Navbar from "./components/layout/Navbar";
+import {BrowserRouter as Router, Route} from "react-router-dom"; 
+
+import Home from "./components/pages/Home";
+import Staff from "./components/pages/Staff";
+
 import './App.css';
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Route path="/" exact={true} component={Home} />
+            <Route path="/staff" exact={true} component={Staff} />
+            <h1>Hello There!!!</h1>
+          </div>
+        </div>
+      </Router>
     );
   }
 }
---
+++ sevafund-okta/src/components/pages/Staff.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class Staff extends Component {
+    render(){
+        return(<h1>This is the Staff page!</h1>);
+    }
+}
+
+export default Staff;
